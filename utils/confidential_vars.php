df4854ace9adc3fcd4130eada4253fa6a3565014
sin2000 <57365134+sin2000@users.noreply.github.com>
2020-10-25 09:55:27 +0100

    update

--- utils/confidential_vars.php
+++ utils/confidential_vars.php
@@ -6,6 +6,10 @@ class confidential_vars
   public static $token_cipher_key_b64 = '';
   public static $wykop_api_appkey = '';
   public static $wykop_api_appsecret = '';
+
+  // path to file without extension
+  public static $user_log_file = '';
+  public static $error_log_file = '';
 }
 
 ?>
