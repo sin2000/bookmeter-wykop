f8a245e586e1ed59a34ba40eafc4e78eb9ae8cbb
sin2000 <57365134+sin2000@users.noreply.github.com>
2021-09-01 17:47:33 +0200

    write error to log file

--- counter.php
+++ counter.php
@@ -1,7 +1,11 @@
 <?php
 
+require_once 'utils/error_log_file.php';
+
 if(!isset($_COOKIE[session_name()]))
 {
+  error_log_file::append('counter.php returned -1. Missing cookie with session name.');
+
   echo '-1';
   return;
 }
